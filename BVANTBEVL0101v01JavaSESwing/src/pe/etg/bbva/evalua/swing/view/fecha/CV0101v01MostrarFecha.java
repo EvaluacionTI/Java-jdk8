e3e99c3e8c82a392a803c9279461e3ecc2b45fa9
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2019-03-29 13:22:47 -0500

    Modulo de Predos Municipales

---
+++ BVANTBEVL0101v01JavaSESwing/src/pe/etg/bbva/evalua/swing/view/fecha/CV0101v01MostrarFecha.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.etg.bbva.evalua.swing.view.fecha;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CV0101v01MostrarFecha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleDateFormat formatoFecha;
+        Date fechaActual = new Date();
+        
+        System.out.println("REPORTE DE FECHAS FORMATEADAS");   
+        formatoFecha = new SimpleDateFormat();
+        System.out.println("Formato según configuración regional:\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha = new SimpleDateFormat("EEEE dd MMMM yyyy");
+        System.out.println("Formato EEEE dd MMMM yyyy:\t\t" + formatoFecha.format(fechaActual));
+                
+        formatoFecha.applyPattern("dd-MMMM-yyyy HH:mm:ss");
+        System.out.println("Formato dd-MMMM-yyyy HH:mm:ss:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("dd-MMM-yyyy HH:mm:ss");
+        System.out.println("Formato dd-MMM-yyyy HH:mm:ss:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("dd-MM-yy HH:mm:ss");
+        System.out.println("Formato dd-MM-yy HH:mm:ss:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("dd/MM/yy HH:mm:ss");
+        System.out.println("Formato dd/MM/yy HH:mm:ss:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("dd-MM-yy hh:mm:ss a");
+        System.out.println("Formato dd-MM-yy hh:mm:ss a:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("dd-MM-yy HH:mm:ss:SS");
+        System.out.println("Formato dd-MM-yy HH:mm:ss:SS:\t\t" + formatoFecha.format(fechaActual));
+        
+        formatoFecha.applyPattern("YYYY/MMM EEEE dd hh:mm a (D");
+        System.out.println("Formato YYYY/MMM EEEE dd hh:mm a D:\t" + formatoFecha.format(fechaActual) + " días del año)");
+    }
+    
+}
