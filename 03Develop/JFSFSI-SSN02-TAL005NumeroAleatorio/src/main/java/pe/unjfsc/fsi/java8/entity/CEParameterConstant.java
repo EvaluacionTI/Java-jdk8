dc4c28003b6c00f4901606a557adbcc1bc22bc2c
Alex Díaz <aldv152027@yahoo.es>
2020-08-15 14:59:06 -0500

    FSI - Taller005 Numero aleatorio 6 digitos

---
+++ 03Develop/JFSFSI-SSN02-TAL005NumeroAleatorio/src/main/java/pe/unjfsc/fsi/java8/entity/CEParameterConstant.java
@@ -0,0 +1,8 @@
+package pe.unjfsc.fsi.java8.entity;
+
+public class CEParameterConstant {
+    public static final int MIN_NUMBER = 100000;
+    public static final int MAX_NUMBER = 999999;
+    public static final int HUK_SECOND = 1000;
+    public static final int PISQA_SECOND = 5000;
+}
---
+++ 03Develop/JFSFSI-SSN02-TAL005NumeroAleatorio/src/main/java/pe/unjfsc/fsi/java8/logical/CLGenerar.java
@@ -0,0 +1,56 @@
+package pe.unjfsc.fsi.java8.logical;
+
+import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static pe.unjfsc.fsi.java8.entity.CEParameterConstant.HUK_SECOND;
+import static pe.unjfsc.fsi.java8.entity.CEParameterConstant.PISQA_SECOND;
+import static pe.unjfsc.fsi.java8.entity.CEParameterConstant.MAX_NUMBER;
+import static pe.unjfsc.fsi.java8.entity.CEParameterConstant.MIN_NUMBER;
+
+public class CLGenerar {
+
+    private static final Logger LOG = LoggerFactory.getLogger("CLGenerar");
+
+    public int generarEntero6Digitos() {
+        Random r = new Random();
+        int Low = MIN_NUMBER;
+        int High = MAX_NUMBER;
+        LOG.info("[EVL] Mínimo : {} ", Low);
+        LOG.info("[EVL] Máximo : {} ", High);
+        int iResult = r.nextInt(High - Low) + Low;
+        LOG.info("[EVL] Numero generado : {} ", iResult);
+        return iResult;
+    }
+
+    public long generar2Segundos() throws InterruptedException {
+        long inicio = System.currentTimeMillis();
+
+        Thread.sleep(PISQA_SECOND);
+
+        long fin = System.currentTimeMillis();
+
+        LOG.info("[EVL] Tiempo inicio : {} ", inicio);
+        LOG.info("[EVL] Tiempo fin    : {} ", fin);
+        long tiempo = (long) ((fin - inicio) / HUK_SECOND);
+
+        LOG.info("[EVL] Número segundos : {}", tiempo);
+        return tiempo;
+    }
+
+    public void generarSecuenciaNumero() throws InterruptedException {
+        boolean flag = false;
+        while (true) {
+            for (int i = 5; i <= 180; i++) {
+                if (flag = false) {
+                    LOG.info("[EVL] Número generado inicial : {}", generarEntero6Digitos());
+                    flag = true;
+                }
+            }
+            if (generar2Segundos() == 5) {
+                LOG.info("[EVL] Número generado : {}", generarEntero6Digitos());
+                flag = false;
+            }
+        }
+    }
+}
